99f6158344d72df0e14f28e40d7efd22203ca83c
EC2 Default User <ec2-user@ip-172-31-62-98.ec2.internal>
2018-02-10 23:09:26 +0000

    finished hw

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,91 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  s = 0
+  arr.each { |a| s +=a }
+  return s
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sorted = arr.sort
+  last_2 = sorted.last(2)
+  if last_2.count == 2 
+    s = last_2[0] + last_2[1]
+  elsif last_2.count == 1
+    s = last_2[0]
+  else
+    s = 0
+  end 
+  return s
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.count < 2
+    return false
+  end
+  arr.each do |a|
+    arr2 = arr
+    arr2.delete_at(arr2.find_index(a))
+    arr2.each do |b|
+      s = a + b
+      if s == n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if /^[^aeiou\W]/i.match(s) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.chars.count < 1
+    return false
+  end
+  s.each_char do |x| 
+    if x !~ /[01]/
+      return false
+    end
+  end
+  back = s.chars.last(2) 
+  back.each do |x|
+    if x !~ /[0]/
+      return false
+    end
+  end
+  return true
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(_isbn, _price)
+    if _isbn.chars.count < 1
+      raise ArgumentError.new("Must be at least 1 character long")
+    end
+    if _price <= 0
+      raise ArgumentError.new("Price must be greater than 0")
+    end
+    @isbn = _isbn
+    @price = _price
+  end
+  attr_accessor :price, :isbn
+  
+  def price_as_string
+    s = "$" + "%.2f" % price
+    return s
+  end
+
 end
